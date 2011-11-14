c6770d03a4f08e2da1aa6e772aa6184a30328e4b
Surya Gaddipati <sgaddipati@groupon.com>
2011-11-13 18:44:54 -0600

    not really important

--- app.rb
+++ app.rb
@@ -7,3 +7,12 @@ get '/' do
   erb :index
 end
 
+get"/:tag" do |tag|
+  episodes_file =  File.join( File.dirname(__FILE__) , 'episodes.yml')
+  @episodes = YAML.load_file(episodes_file)["episodes"].select do |episode|
+    episode["tags"].include?(tag)
+  end
+  erb :index
+
+end
+
